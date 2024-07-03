9519a7980807284b43a3dc439d17722929fdcfb4
Prajwal Kumar <prasunjaysjc@gmail.com>
2024-07-03 19:06:04 +0530

    changed directories

--- server/backend.py
+++ server/backend.py
@@ -6,20 +6,6 @@ app = Flask(__name__)
 
 def detect_dark_patterns(texts):
 
-    '''
-    texts format:
-    [
-        {
-            "id" : "generated-id-123",
-            "text" : "Hurry Now! Offer closes in 15 minutes"
-        },
-        {
-            "id":"generated-id-123",
-            "text":"Hurry Now! Offer closes in 15 minutes"
-        }
-    ]
-    '''
-
     dark_pattern_ids = []
 
     text = [item['text'] for item in texts]
@@ -33,13 +19,13 @@ def detect_dark_patterns(texts):
     rf_predictions = rf_predictions.tolist()
     svm_predictions = svm_predictions.tolist()
 
-    # Assume the ML model returns a list of IDs that have dark patterns
     for id_, rf_pred, svm_pred in zip(ids, rf_predictions, svm_predictions):
-        dark_pattern_ids.append({
-            'id': id_,
-            'random_forest_prediction': category_map[int(rf_pred)],
-            'svm_prediction': category_map[int(svm_pred)] 
-        })
+        if not(rf_pred == 1 and svm_pred == 1):
+            dark_pattern_ids.append({
+                'id': id_,
+                'random_forest_prediction': category_map[int(rf_pred)],
+                'svm_prediction': category_map[int(svm_pred)] 
+            })
     return dark_pattern_ids
 
 @app.route('/check_dark_patterns', methods=['POST'])
@@ -47,6 +33,7 @@ def check_dark_patterns():
     data = request.get_json()
     texts = data['texts']
     dark_pattern_ids = detect_dark_patterns(texts)
+    print(dark_pattern_ids)
     return jsonify(darkPatternIds=dark_pattern_ids)
 
 if __name__ == '__main__':
