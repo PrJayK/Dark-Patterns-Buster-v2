d4c651c3642bb85a5ed20ae098ab1356579452ff
Hardik <119474920+hardikk65@users.noreply.github.com>
2024-07-03 17:05:33 +0530

    Updated (#2)
    * Updated
    
    * Updated Again

--- server/backend.py
+++ server/backend.py
@@ -1,14 +1,45 @@
 from flask import Flask, request, jsonify
+import json
+from model.load_models import random_forest_classifier,svm_classifier,vect,category_map
 
 app = Flask(__name__)
 
-# Your ML model for detecting dark patterns
 def detect_dark_patterns(texts):
+
+    '''
+    texts format:
+    [
+        {
+            "id" : "generated-id-123",
+            "text" : "Hurry Now! Offer closes in 15 minutes"
+        },
+        {
+            "id":"generated-id-123",
+            "text":"Hurry Now! Offer closes in 15 minutes"
+        }
+    ]
+    '''
+
     dark_pattern_ids = []
+
+    text = [item['text'] for item in texts]
+    ids = [item['id'] for item in texts]
+
+    x_test = vect.transform(text)
+
+    rf_predictions = random_forest_classifier.predict(x_test)
+    svm_predictions = svm_classifier.predict(x_test)
+
+    rf_predictions = rf_predictions.tolist()
+    svm_predictions = svm_predictions.tolist()
+
     # Assume the ML model returns a list of IDs that have dark patterns
-    for text in texts:
-        if your_ml_model.predict(text['text']):
-            dark_pattern_ids.append(text['id'])
+    for id_, rf_pred, svm_pred in zip(ids, rf_predictions, svm_predictions):
+        dark_pattern_ids.append({
+            'id': id_,
+            'random_forest_prediction': category_map[int(rf_pred)],
+            'svm_prediction': category_map[int(svm_pred)] 
+        })
     return dark_pattern_ids
 
 @app.route('/check_dark_patterns', methods=['POST'])
